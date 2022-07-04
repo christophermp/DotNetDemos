00834148c170ae7c3c4c7e14ac52aacda614bf95
Christopher Meldre Pettersen <meldre@outlook.com>
2022-07-04 14:23:26 +0200

    update

--- BattleshipLiteApp/BattleshipLite/Program.cs
+++ BattleshipLiteApp/BattleshipLite/Program.cs
@@ -14,10 +14,38 @@ namespace BattleshipLite
         {
             WelcomeMessage();
 
-            PlayerInfoModel player1 = CreatePlayer();
-            PlayerInfoModel player2 = CreatePlayer();
+            PlayerInfoModel activePlayer = CreatePlayer("Player 1");
+            PlayerInfoModel opponent = CreatePlayer("Player 2");
+            PlayerInfoModel winner = null;
+
+            do
+            {
+                // Display grid from activePlayer on where they fired.
+                DisplayShotGrid(activePlayer);
+
+                // Ask activePlayer for a shot.
+                // Determine if it is a valid shot.
+                // Determine shot results.
+                // Determine if the game is over.
+                // If over, set activePlayer as the winner.
+                // else, swap positions (activePlayer to opponent).
+
+            } while (winner == null);
 
             Console.ReadLine();
+        }
+
+        private static void DisplayShotGrid(PlayerInfoModel activePlayer)
+        {
+            string currentRow = activePlayer.ShotGrid[0].SpotLetter;
+
+            foreach (var gridSpot in activePlayer.ShotGrid)
+            {
+                if (gridSpot.Status == GridSpotStatus.Empty)
+                {
+                    Console.Write($" {gridSpot.SpotLetter}{gridSpot.SpotNumber} ");
+                }
+            }
         }
 
         private static void WelcomeMessage()
@@ -27,9 +55,11 @@ namespace BattleshipLite
             Console.WriteLine();
         }
 
-        private static PlayerInfoModel CreatePlayer()
+        private static PlayerInfoModel CreatePlayer(string playerTitle)
         {
             PlayerInfoModel output = new PlayerInfoModel();
+
+            Console.WriteLine($"Player information for {playerTitle}");
 
             // Ask the user for their name
             output.UsersName = AskForUsersName();
